d4d5ec8f1cf25e16f2c385aa3f5a77ffd5afbdf4
szpone <nikola.adamus@gmail.com>
2019-08-07 19:38:04 +0200

    add recommendations handler

--- api.py
+++ api.py
@@ -1,6 +1,7 @@
-from typing import Any
+from typing import Any, Dict
 
 import arrow
+import requests
 from tornado.ioloop import IOLoop
 from tornado.web import Application, RequestHandler
 
@@ -11,9 +12,26 @@ class CurrentTimeHandler(RequestHandler):
         self.write({"message": f"{utc}"})
 
 
-def make_app():
-    urls: Any = [("/", CurrentTimeHandler)]
-    return Application(urls)
+class RecommendationsHandler(RequestHandler):
+    def post(self) -> None:
+        api_key: str = "f53dd4aea5bfc8ecd850fcbe1b08921e"
+        url: str = "https://news-api.lateral.io/documents/similar-to-text"
+        payload: str = '{"text": "machine learning is" }'
+        headers: Dict[str, Any] = {
+            "subscription-key": f"{api_key}",
+            "content-type": "application/json",
+        }
+        response = requests.request("POST", url, data=payload, headers=headers)
+
+        self.write({"message": f"{response.text}"})
+
+
+def make_app() -> Application:
+    urls: Any = [
+        ("/", CurrentTimeHandler),
+        ("/recommendations", RecommendationsHandler),
+    ]
+    return Application(urls, debug=True)
 
 
 if __name__ == "__main__":
