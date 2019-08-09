294f24e4bf451fa6bdc12404981bb0f008bc3a02
szpone <nikola.adamus@gmail.com>
2019-08-09 20:49:47 +0200

    handle request body

--- api.py
+++ api.py
@@ -4,13 +4,14 @@ import arrow
 import requests
 from tornado.ioloop import IOLoop
 from tornado.web import Application, RequestHandler
+import json
 
 
 class CurrentTimeHandler(RequestHandler):
     def set_default_headers(self):
         self.set_header("Access-Control-Allow-Origin", "*")
         self.set_header("Access-Control-Allow-Headers", "x-requested-with")
-        self.set_header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
+        self.set_header('Access-Control-Allow-Methods', 'GET, OPTIONS')
 
     def get(self) -> None:
         current_time = arrow.now().format("YYYY-MM-DD HH:mm:ss")
@@ -18,17 +19,26 @@ class CurrentTimeHandler(RequestHandler):
 
 
 class RecommendationsHandler(RequestHandler):
+    def set_default_headers(self):
+        self.set_header("Access-Control-Allow-Origin", "*")
+        self.set_header("Access-Control-Allow-Headers", "content-type")
+        self.set_header('Access-Control-Allow-Methods', 'POST, OPTIONS')
+
     def post(self) -> None:
         api_key: str = "f53dd4aea5bfc8ecd850fcbe1b08921e"
         url: str = "https://news-api.lateral.io/documents/similar-to-text"
-        payload: str = '{"text": "machine learning is" }'
+        payload: Dict[str, Any] = json.loads(self.request.body)
         headers: Dict[str, Any] = {
             "subscription-key": f"{api_key}",
             "content-type": "application/json",
         }
-        response = requests.request("POST", url, data=payload, headers=headers)
+        response = requests.request("POST", url, data=payload['searchText'], headers=headers)
 
         self.write({"message": f"{response.text}"})
+
+    def options(self):
+        self.set_status(200)
+        self.finish()
 
 
 def make_app() -> Application:
